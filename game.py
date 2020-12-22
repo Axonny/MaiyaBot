2f2c2cf18b12a7bb60fe8140ea938e3a096c5d98
Axonny <KawaiiAxonny@gmail.com>
2020-12-22 20:31:17 +0500

    add game

---
+++ game.py
@@ -0,0 +1,43 @@
+from PIL import Image,ImageDraw
+from io import BytesIO
+from ImageParser import YandexImage
+import requests
+import random
+
+parser = YandexImage()
+
+def save(name, link):
+    with open(f"test/{name}","wb") as f:
+        b = requests.get(link)
+        f.write(b.content)
+
+def generate_name():
+    alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789"
+    return "".join(random.choices(alphabet, k=20))
+
+def get_img(name):
+    images = parser.search(name, parser.size.large)
+    name = generate_name() + ".jpg"
+    save(name, images[random.randint(0, len(images))].url)
+    return name
+
+def resize_image(input):
+    image = Image.open(input)
+    width, height = image.size
+    draw = ImageDraw.Draw(image)
+    pix = image.load()
+    for i in range(width):
+        for j in range(height):
+            a = pix[i, j][0]
+            b = pix[i, j][1]
+            c = pix[i, j][2]
+            S = (a + b + c) // 3
+            draw.point((i, j), (S, S, S))
+    resize = image.resize((int(width*0.05),int(height*0.05)))
+    resize = resize.resize((int(width),int(height)))
+
+    bio = BytesIO()
+    bio.name = 'image.jpeg'
+    resize.save(bio, 'JPEG')
+    bio.seek(0)
+    return bio
--- main.py
+++ main.py
@@ -1,32 +1,52 @@
-import time
 import telebot
+import game
+import os
+import random
 from telebot import types
-from datetime import datetime, timedelta
-from threading import Thread
 from config import Config
 
 # Global variables
 cfg = Config()
 bot = telebot.TeleBot(cfg.token)
 myID = cfg.get_data_str("myID")
+users = {}
+words = []
+with open("words.txt", "r", encoding='utf-8') as f:
+    words = [i.strip() for i in f.readlines()]
 
-# <Markups>
 
-# generate methods
 
 def ReplyKeyboardGenerate(*args):
     for i in args:
         yield types.KeyboardButton(i)
 
-# Main markups
+mainMarkup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+items = ReplyKeyboardGenerate("Играть")
+mainMarkup.add(*[i for i in items])
 
-# </Markups>
+gameMarkup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+items = ReplyKeyboardGenerate("Закончить")
+gameMarkup.add(*[i for i in items])
 
-# <BotMethods>
+
+
+def send_next_img(chat_id):
+    img_name = words[random.randint(0, len(words))]
+    name = game.get_img(img_name)
+    bot.send_photo(chat_id, photo=game.resize_image(f"img/{name}"))
+    users[chat_id]["last_img"] = name
+    users[chat_id]["answer"] = img_name
+
+def clear_img(name):
+    try:
+        os.remove(f"img/{name}")
+    except:
+        pass
+
+
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-
     bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\n\
 Я - <b>{1.first_name}</b>, бот созданный @KawaiiAxonny.".format(message.from_user, bot.get_me()),
 		parse_mode='html', reply_markup=(mainMarkup if message.chat.id == myID else None))
@@ -35,9 +55,36 @@ def welcome(message):
 def mes(message):
     if message.chat.type == 'private':
         mes = message.text.lower()
-        chatId = message.chat.id
+        chat_id = message.chat.id
 
-# </BotMethods>
+        if not chat_id in users:
+            users[chat_id] = {
+                "in_game": False,
+                "last_img": "",
+                "answer": ""
+            }
+
+        if mes == "играть":
+            bot.send_message(chat_id, "Начинаем игру...", reply_markup=gameMarkup)
+            users[chat_id]["in_game"] = True
+            send_next_img(chat_id)
+
+        elif users[chat_id]["in_game"] and mes == "закончить":
+            bot.send_message(chat_id, "Игра закончена", reply_markup=mainMarkup)
+            users[chat_id]["in_game"] = False
+            clear_img(users[chat_id]["last_img"])
+
+        elif users[chat_id]["in_game"]:
+            bot.send_photo(chat_id, photo=open(f'img/{users[chat_id]["last_img"]}', "rb"))
+            if users[chat_id]["answer"] == mes:
+                bot.send_message(chat_id, "Правильно", reply_markup=gameMarkup)
+            else:
+                bot.send_message(chat_id, f'Неправильно, это {users[chat_id]["answer"]}', reply_markup=gameMarkup)
+            clear_img(users[chat_id]["last_img"])
+            send_next_img(chat_id)
+
+        else:
+            bot.send_message(chat_id, 'чтобы сыграть отправь мне слово "Играть"', reply_markup=mainMarkup)
 
 # RUN
 bot.polling(none_stop=True)
